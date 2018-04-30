19f2325640ebf5de729272baaf0d5b97cd9b8fbe
Babala <759160019>
2018-04-30 21:53:47 +0800

    加载主界面

--- Transpic/app/src/main/java/com/example/lenovo/transpic/MainActivity.java
+++ Transpic/app/src/main/java/com/example/lenovo/transpic/MainActivity.java
@@ -2,82 +2,59 @@ package com.example.lenovo.transpic;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private ImageButton ImgBtnAlbum;
+    private ImageButton ImgBtnCamera;
 
-    private ImageButton fun1;
-    private ImageButton  fun2;
-    private ImageButton  fun3;
-    private ImageButton  fun4;
-
+    /*    private ImageView iv_recycle;
+    AnimationDrawable ivRecycleAnimation;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fun1=(ImageButton )findViewById(R.id.imagebutton1);
-        fun2=(ImageButton )findViewById(R.id.imagebutton2);
-        fun3=(ImageButton )findViewById(R.id.imagebutton3);
-        fun4=(ImageButton )findViewById(R.id.imagebutton4);
+        ImgBtnAlbum = (ImageButton)findViewById(R.id.img_btn_album);
+        ImgBtnCamera = (ImageButton)findViewById(R.id.img_btn_camera);
 
-        //下面为各个按钮设置事件监听
-        fun1.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, GSPic.class);
-                i.putExtra("data", "fun1");// 传递参数
-                startActivity(i);
-            }
-        });
+        ImgBtnAlbum.setOnClickListener(this);
+        ImgBtnCamera.setOnClickListener(this);
 
-        fun2.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                Intent o = new Intent(MainActivity.this, GSPic.class);
-                //o.putExtra("data", "fun2");// 传递参数
-                startActivity(o);
-            }
-        });
-
-        fun3.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                Intent j = new Intent(MainActivity.this, GSPic.class);
-                //j.putExtra("data", "fun3");// 传递参数
-                startActivity(j);
-            }
-        });
-
-        fun4.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                Intent k = new Intent(MainActivity.this, GSPic.class);
-                k.putExtra("data", "fun4");// 传递参数
-                startActivity(k);
-
-            }
-        });
+        /*iv_recycle = (ImageView)findViewById(R.id.iv_recycle);
+        iv_recycle.setBackgroundResource(R.drawable.img_anim);
+        ivRecycleAnimation = (AnimationDrawable)iv_recycle.getBackground();
+        ivRecycleAnimation.start();*/
 
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.img_btn_album:
+                //选择相册
 
+                break;
 
+            case R.id.img_btn_camera:
+               //相机拍照
 
+                break;
 
-
+                default:
+                    break;
+        }
+    }
 }
