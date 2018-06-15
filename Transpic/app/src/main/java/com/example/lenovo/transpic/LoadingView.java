d5fc41ba96756dc2c934e56ac2261e2200ca91b8
Med <925314768@qq.com>
2018-06-15 18:38:12 +0800

    Revert "加载动画"
    This reverts commit 5b518205b6ebcd42e8ce5bf83788114179041841.

--- Transpic/app/src/main/java/com/example/lenovo/transpic/LoadingView.java
+++ Transpic/app/src/main/java/com/example/lenovo/transpic/LoadingView.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
